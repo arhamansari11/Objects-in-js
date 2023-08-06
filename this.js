2e90bbd4fc7edabc55ef5ee100db7920350445be
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2023-08-06 15:42:58 +0500

    Add files via upload

---
+++ this.js
@@ -0,0 +1,112 @@
+document.write("<h2>This is the practice of objects and their properties.</h2>")
+
+
+// let myobj = {
+//     myname : "Arham",
+//     class : 12,
+//     fname : "Nadeem",
+//     Boolean : true
+// };
+
+// document.write(  "My name is " ,myobj["myname"]," I read in ", myobj["class"], " My father name is ", myobj["fname"] ," I am a " , myobj["Boolean"], " Freind   <br/> <br/> <br/>" )
+
+
+// let testing = {
+//     car1 : "Ferrari",
+//     car2 : "BMW",
+//     car3 : "Sportage",
+//     car4 : "Civic"
+// }
+
+// console.log('car3' in testing);
+
+// for (let key in testing) {
+//     console.log(key + " : " + testing[key])
+// }
+
+
+// testing["car5"] = "City";
+
+//     delete testing["car3"];
+
+// console.log(testing["car1"],testing["car2"],testing["car3"],testing["car4"],testing["car5"])
+
+
+
+
+
+// let testing = {
+//     car1 : "Ferrari",
+//     car2 : "BMW",
+//     car3 : "Sportage",
+//     car4 : "Civic",
+
+
+    // greet : function () {
+    //     console.log("Hello World")
+    // }
+
+
+//     greet (){
+//         console.log("This method is introduced in ES6")
+//     }
+// }
+
+
+// testing.greet = function(){
+//     console.log("This object say Hello!")
+// }
+
+
+// function hello() {
+//     console.log("This Simple function in object says HELLO!")
+// }
+// testing.greet = hello;
+
+
+// testing.greet();
+
+// let myslef = {
+//     name1 : "Arham",
+//     name2 : "Nadeem",
+//     thisfunction (){
+//         console.log(`Hello! I am Muhammad ${this.name1 } ${this.name2}. I like the ${brand.brand}.Model ${brand.model}  `)
+//     }
+// }
+
+
+
+// myslef.age = 18;
+
+
+
+
+
+//  let brand = {
+//     brand : "Honda",
+//     model : "Civic"
+//  }
+
+//  myslef.thisfunction();
+
+
+
+
+
+// let x = Math.floor(Math.random() * 10) + 15;
+
+// console.log(x)
+
+
+// let practice = new Object ();
+
+// practice.name = "Arham Nadeem"
+// practice.age = 18;
+
+// console.log(practice.name , practice.age)
+
+
+
+
+
+
